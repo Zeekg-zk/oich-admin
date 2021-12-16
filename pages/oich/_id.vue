05bb3b570e379f8bc19ee8ae2b37e0a6da072fd2
ZiKng-Coding <3193373997@qq.com>
2021-12-16 22:06:00 +0800

    update

--- pages/oich/_id.vue
+++ pages/oich/_id.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="project">
     <h1 style="text-align: center;">
-      苗族古歌
+      Miao ancient songs
     </h1>
     <!-- 项目相关信息 -->
     <table class="project-info">
       <tbody>
         <tr>
           <td>公布时间：2006(第一批)</td>
-          <td>类别：民间文学</td>
+          <td>Class：民间文学</td>
         </tr>
         <tr>
-          <td>项目等级：省级</td>
-          <td>保护单位：台江县非物质文化遗产保护中心</td>
+          <td>Project level：省级</td>
+          <td>Protection unit</td>
         </tr>
         <tr>
-          <td>传承人：田锦锋  张定强  刘永洪  王安江 </td>
+          <td>Persons</td>
         </tr>
       </tbody>
     </table>
     <!-- 项目视频（如果有的话） -->
     <div v-if="hasVideo" class="project-video">
-      <h2>影音介绍</h2>
+      <h2>video Introduce</h2>
       <div class="video">
         <ProjectVideo :poster="posterUrl" :sources="sourcesOjb" />
       </div>
@@ -31,14 +31,14 @@
       <h2>详细介绍</h2>
       <div class="article">
         <p>
-          苗族分布在我国西南数省区。按方言划分，大致可分为湘西方言区、黔东方言区、川滇黔方言区。黔东南清水江流域一带是全国苗族最大的聚居区，大致包括凯里、剑河、黄平、台江、雷山、丹寨、施秉、黄平、镇远、三穗，以及广西三江和湖南靖县等地。在此广大苗族聚居区普遍流传着一种以创世为主体内容的诗体神话，俗称“古歌”或“古歌古词”。
+          React can change how you think about the designs you look at and the apps you build. Where once you might have seen a forest, after working with React, you will appreciate the individual trees. React makes it easier to think in design systems and UI states. In this tutorial, we’ll guide you through the thought process of building a searchable product data table with React.
         </p>
         <img class="img" src="https://img0.baidu.com/it/u=1814334693,2710732047&fm=26&fmt=auto" alt="1">
         <p>
-          苗族古歌古词神话大多在鼓社祭、婚丧活动、亲友聚会和节日等场合演唱，演唱者多为中老年人、巫师、歌手等。酒席是演唱古歌的重要场合。苗族的古歌古词神话是一个民族的心灵记忆，是苗族古代社会的百科全书和“经典”，具有史学、民族学、哲学、人类学等多方面价值。今天，这些古歌古词神话还在民间流传唱诵。
+          React can change how you think about the designs you look at and the apps you build. Where once you might have seen a forest, after working with React, you will appreciate the individual trees. React makes it easier to think in design systems and UI states. In this tutorial, we’ll guide you through the thought process of building a searchable product data table with React.
         </p>
         <p>
-          但由于受到现代文化和市场经济的冲击，苗族古歌已濒临失传。以台江为例，在全县13万苗族同胞中，能唱完整部古歌的已寥寥无几，目前只有二百余人能唱一些不完整的古歌，而且都是中老年人，传承古歌较多的老人年事已高。如不抓紧抢救保护，苗族古歌这一民族瑰宝将最终在世间消失。
+          React can change how you think about the designs you look at and the apps you build. Where once you might have seen a forest, after working with React, you will appreciate the individual trees. React makes it easier to think in design systems and UI states. In this tutorial, we’ll guide you through the thought process of building a searchable product data table with React.
         </p>
       </div>
     </div>
